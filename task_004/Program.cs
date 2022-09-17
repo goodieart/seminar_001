e3590d77e3b5e53c316bd9547aeb3aacf3101814
Pavel Yastremsky <goodieart@gmail.com>
2022-09-18 02:11:42 +0300

    task_004 completed

---
+++ task_004/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+WriteLine("Введите числа a,b,c: ");
+int max = int.Parse(ReadLine()!);
+int b = int.Parse(ReadLine()!);
+int c = int.Parse(ReadLine()!);
+
+if(b > max) max = b;
+if(c > max) max = c;
+
+WriteLine($"max = {max}");
