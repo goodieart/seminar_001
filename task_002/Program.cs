6243f79b0c218d05cadda77f189716bfefa9ca25
Pavel Yastremsky <goodieart@gmail.com>
2022-09-18 14:07:25 +0300

    task_002 fixed

--- task_002/Program.cs
+++ task_002/Program.cs
@@ -1,9 +1,14 @@
 ﻿using static System.Console;
 
 WriteLine("Введите числа a,b: ");
-int max = int.Parse(ReadLine()!);
-int b = int.Parse(ReadLine()!);
+int a = int.Parse(ReadLine()!);         // Стараемся не использовать дополнительные переменные
+int b = int.Parse(ReadLine()!);         // (экономим память);
 
-if(b > max) max = b;
+if(b > a)
+{
+    a = a + b;                          // Здесь используем "арифметический" swap;
+    b = a - b;                          // Возможная проблема в продакшене: stack overflow
+    a = a - b;                          // (при значениях переменных, близких к пределам размерности типа);
+}
 
-WriteLine(max);
+WriteLine($"max = {a}; min = {b}");
